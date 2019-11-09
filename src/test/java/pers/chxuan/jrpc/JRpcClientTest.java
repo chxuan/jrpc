2b793167e4960c33fe09959599a246b4ef24675a
chxuan <787280310@qq.com>
2019-11-09 11:42:23 +0800

    update code

---
+++ src/test/java/pers/chxuan/jrpc/JRpcClientTest.java
@@ -0,0 +1,13 @@
+package pers.chxuan.jrpc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JRpcClientTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JRpcClientTest.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("JRpcClientTest");
+    }
+}
